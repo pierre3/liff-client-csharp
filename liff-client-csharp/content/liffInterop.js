54633030bf90cd6124317f4bd06a8402bb9e9f01
pierre3 <pierre3@outlook.com>
2019-08-04 11:38:13 +0900

    _

--- liff-client-csharp/content/liffInterop.js
+++ liff-client-csharp/content/liffInterop.js
@@ -1,6 +1,11 @@
 ﻿window.liffInterop = {
     init: function () {
         return new Promise(function (resolve, reject) {
+            if (!liff)
+            {
+                reject("LIFF object not ready.");
+                return;
+            }
             liff.init(
                 function (data) {
                     resolve(JSON.stringify(data));
